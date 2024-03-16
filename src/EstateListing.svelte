5454de76754ba8bee5ff30b84feb67ba2feaa39a
Tiago Faucz <tifaucz@gmail.com>
2024-03-16 19:40:17 -0300

    add initial listing and types

---
+++ src/EstateListing.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import EstateCard from './EstateCard.svelte';
+  import type { Listing } from './types';
+
+  export let listings: Listing[];
+</script>
+
+<ul class="flex flex-wrap -mx-4">
+  {#each listings as listing}
+    <li class="px-4 mb-4 flex-auto">
+      <EstateCard {listing} />
+    </li>
+  {/each}
+</ul>
---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export type Listing = {
+    Id: number;
+    DateListed: string;
+    Title: string;
+    Description: string;
+    "Sale Price": number;
+    ThumbnailURL: string;
+    PictureURL: string;
+    Location: string;
+    Sqft: number;
+    Bedrooms: number;
+    Bathrooms: number;
+    Parking: number;
+    YearBuilt: number;
+  };
+  
